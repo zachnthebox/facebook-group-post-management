5c529c66ca44892f410a9327d2b75008f2b374cb
Zachary Berardo <zachary@zachnthebox.com>
2016-04-23 18:52:56 -0400

    :white_check_mark: tests: added tests for add-posts component

--- tests/integration/components/add-post/component-test.js
+++ tests/integration/components/add-post/component-test.js
@@ -1,24 +1,38 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
+import { openDatepicker } from 'ember-pikaday/helpers/pikaday';
+import moment from 'moment';
 
 moduleForComponent('add-post', 'Integration | Component | add post', {
   integration: true
 });
 
-test('it renders', function(assert) {
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
+test('validation works', function(assert) {
+  assert.expect(2);
 
   this.render(hbs`{{add-post}}`);
 
-  assert.equal(this.$().text().trim(), '');
+  this.$('.text-muted').each(function() {
+    if ($(this).text()) {
+      assert.ok(true);
+    }
+  });
+});
 
-  // Template block usage:
-  this.render(hbs`
-    {{#add-post}}
-      template block text
-    {{/add-post}}
-  `);
+test('add post', function(assert) {
+  const postDate = moment().add(4, 'days').startOf('day').toDate();
+  this.set('addPostAction', post => {
+    assert.equal(post.text, 'foo');
+    assert.equal(post.type, 'scripture');
+    assert.ok(moment(post.date).isSame(postDate));
+  });
+  this.render(hbs`{{add-post action=(action addPostAction)}}`);
 
-  assert.equal(this.$().text().trim(), 'template block text');
+  let inputs = this.$('input');
+  let interactor = openDatepicker(inputs.eq(2));
+
+  interactor.selectDate(postDate);
+  inputs.eq(0).val('foo').change();
+
+  this.$('button').click();
 });
